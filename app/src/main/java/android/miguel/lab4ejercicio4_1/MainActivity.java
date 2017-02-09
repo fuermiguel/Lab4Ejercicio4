aa376dc2ea3c1921a08d567c4e5c0414f99c89e2
fuermiguel <fuermiguel@yahoo.es>
2017-02-09 20:40:51 +0000

    nada nuevo, limpiar un poco

--- app/src/main/java/android/miguel/lab4ejercicio4_1/MainActivity.java
+++ app/src/main/java/android/miguel/lab4ejercicio4_1/MainActivity.java
@@ -9,10 +9,9 @@ import android.provider.ContactsContract.Contacts.Entity;
 import android.provider.ContactsContract.RawContacts;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.provider.ContactsContract.RawContactsEntity;
 
 public class MainActivity extends AppCompatActivity {
-//todo hay que crear un string e ir añadiendole la información en cada sitio
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
         TextView salida = (TextView) findViewById(R.id.salida);
 
-        /*
-            Obtenemos un cursor con los contactos
-         */
+        //Obtenemos un cursor con los contactos
+
         Cursor contactos = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI,
                 null, null, null, null);
 
@@ -33,13 +31,13 @@ public class MainActivity extends AppCompatActivity {
         */
         while (contactos.moveToNext()) {
             String cadena = "\n"
-                    +"NOMBRE"
+                    + "NOMBRE"
                     + "\n"
                     + contactos.getString(contactos.getColumnIndex(Contacts.DISPLAY_NAME_PRIMARY))
                     + "\n";
-            /*
-                Añadimos al uri el número id del contacto
-             */
+
+            // Añadimos al uri el número id del contacto
+
             Uri contactUri = Uri.withAppendedPath(Contacts.CONTENT_URI,
                     contactos.getString(contactos.getColumnIndex(Contacts._ID)));
             /*
@@ -51,18 +49,18 @@ public class MainActivity extends AppCompatActivity {
             /*
             Este cursor contiene la entity del contacto (solo un registro)
              */
-            Cursor entidaContacto = getContentResolver().query(contactUri,
+            Cursor contactoEntity = getContentResolver().query(contactUri,
                     null, null, null, null);
-            entidaContacto.moveToFirst();
+            contactoEntity.moveToFirst();
             /*
             Obtenemos un cursor con los rawsContacts para un contact determinado(Puede ser más de un registro)
              */
             Cursor contactosRaw = getContentResolver().query(RawContacts.CONTENT_URI,
                     null,
                     RawContacts.CONTACT_ID + "=?",
-                    new String[]{entidaContacto.getString(entidaContacto.getColumnIndex(Entity.RAW_CONTACT_ID))},
+                    new String[]{contactoEntity.getString(contactoEntity.getColumnIndex(Entity.RAW_CONTACT_ID))},
                     null);
-            
+
             //Por cada rawContact hay que obtener los DATA desde un rawContactEntity(union de tablas rawContact y data)
             while (contactosRaw.moveToNext()) {
                 cadena = cadena
@@ -75,34 +73,39 @@ public class MainActivity extends AppCompatActivity {
                     parámetros _ID=Contacts.Entity.RAW_CONTACT y DATA_ID = Contacts.Entity.DATA_ID
                  */
 
-                 /*
-                    Añadimos al uri el número id del contactoRAW
-                */
+
+                // Añadimos al uri el número id del contactoRAW
+
                 Uri contactRawUri = Uri.withAppendedPath(RawContacts.CONTENT_URI,
                         contactos.getString(contactos.getColumnIndex(Contacts._ID)));
-                /*
-                    Añadimos el entity al ury para conformar el uri consultar
-                 */
+
+                // Añadimos el entity al ury para conformar el uri consultar
+
                 contactRawUri = Uri.withAppendedPath(contactRawUri,
                         RawContacts.Entity.CONTENT_DIRECTORY);
 
                 /*
-                    Aqui obtengo todos los detalles para un contacto y cuenta conocidas.
+                    Aqui obtengo todos los detalles para un contacto(contact.Entity) y
+                     cuenta conocidas contact.RawContactEntity).
                  */
                 Cursor entidadContactoRaw = getContentResolver().query(contactRawUri,
                         new String[]{
-                                ContactsContract.RawContactsEntity._ID,
-                                ContactsContract.RawContactsEntity.DATA_ID,
-                                ContactsContract.RawContactsEntity.MIMETYPE,
-                                ContactsContract.RawContactsEntity.DATA1,
-                                ContactsContract.RawContactsEntity.DATA2,
-                                ContactsContract.RawContactsEntity.DATA3},
-                       //ContactsContract.RawContactsEntity._ID + "=?" + " AND " + ContactsContract.RawContactsEntity.DATA_ID + "=?",
-                       // new String[]{entidaContacto.getString(entidaContacto.getColumnIndex(Entity.RAW_CONTACT_ID)),
-                       //         entidaContacto.getString(entidaContacto.getColumnIndex(Entity.DATA_ID))},
+                                RawContactsEntity._ID,
+                                RawContactsEntity.DATA_ID,
+                                RawContactsEntity.MIMETYPE,
+                                RawContactsEntity.DATA1,
+                                RawContactsEntity.DATA2,
+                                RawContactsEntity.DATA3},
                         null,
                         null,
                         null);
+
+                /*
+                    Ahora podemos mostrar los datos que necesitemos (tener en cuenta las columnas
+                    de entidadContactoRaw para incluir las que necesitemos. Lo tengo a lo bruto, pero
+                    habría que filtrar por mimeType y coger los datos que necesitemos dependiendo
+                    de cada caso.
+                 */
                 try {
                     while (entidadContactoRaw.moveToNext()) {
                         String sourceId = entidadContactoRaw.getString(0);
@@ -112,7 +115,7 @@ public class MainActivity extends AppCompatActivity {
                             String data2 = entidadContactoRaw.getString(4);
                             String data3 = entidadContactoRaw.getString(4);
 
-                            cadena =  cadena + "mimeType:" + "\n"
+                            cadena = cadena + "mimeType:" + "\n"
                                     + mimeType + "\n"
                                     + "DATA1" + "\n"
                                     + data1 + "\n"
